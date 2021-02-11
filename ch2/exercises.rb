874c3949061b1709de8f40a8148014c6f5981c33
EC2 Default User <ec2-user@ip-172-31-20-197.us-west-2.compute.internal>
2021-02-11 07:07:30 +0000

    completed ch1/ch2 exercises

---
+++ ch2/exercises.rb
@@ -0,0 +1,56 @@
+# Exercise 1
+
+first_name = "Steven "
+last_name = "Yean"
+
+name = first_name + last_name
+
+puts name
+
+# Exercise 2
+
+ e1 = 4321 / 1000
+ e2 = 4321 % 1000 / 100
+ e3 = 4321 % 100 / 10
+ e4 = 4321 % 10 / 1
+ puts "thousands: #{e1}", "hundreds: #{e2}", "tens: #{e3}", "ones: #{e4}"
+ 
+ # Exercise 3
+ 
+ anime = {
+     "Monthly Girl's Nozaki-Kun" => 2014,
+     "Oregairu" => 2013,
+     "Seishun Buta Yarou" => 2018,
+     "Railgun T" => 2020,
+     "Love is War Season 2" => 2019
+ }
+ 
+ puts anime["Monthly Girl's Nozaki-Kun"]
+ puts anime["Oregairu"]
+ puts anime["Seishun Buta Yarou"]
+ puts anime["Railgun T"]
+ puts anime["Love is War Season 2"]
+ 
+ # Exercise 4
+ 
+ years = [2014, 2013, 2018, 2020, 2019]
+ 
+puts years
+
+# Exercise 5
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# Exercise 6
+
+puts 3.0 * 3
+puts 100.0 * 100
+puts 55.0 * 55
+
+# Exercise 7
+
+# This means that instead of a parantheses, we are supposed to use a curly bracket. A closing bracket does not follow one of our statements.
+
